5735f9caf9174277ed09b87a76b60be5589cc6f4
MARCEL RODRIGUES DE MORAES PENHA <marcel.rmpenha@senacsp.edu.br>
2023-09-27 16:51:24 -0300

    Terceiro commit

--- SQL/testeImagem.sql
+++ SQL/testeImagem.sql
@@ -2,12 +2,17 @@ create database cadastro;
 
 use cadastro;
 
+drop table pessoa;
+
 create table pessoa(
     codigo int primary key auto_increment not null,
     nome varchar(150) not null,
+    unidade varchar(150) not null,
+    CPF bigint(20) not null,
+    cidade varchar(150) not null,
+    estado varchar(150) not null,
+    celular int not null,
     foto longblob not null
 )engine=innodb; 
 
-
-
-
+select * from pessoa;
